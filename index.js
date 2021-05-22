3081ba862afc75ad568a7d52fca216fb3637839e
Pramod <server@parkupnow.com>
2021-05-22 09:07:08 +0530

    sequelize for multi environment

--- index.js
+++ index.js
@@ -2,20 +2,7 @@ var http = require('http');
 const express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
-const { Sequelize } = require('sequelize');
 require('dotenv').config()
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DIALECT
-});
-
-try {
-  sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
 
 var schema = buildSchema(`
   type Query {
